a52522082449ae3a7e8adcf3113aff63e1e74ad1
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-02-09 02:14:38 +0530

    add comprehensive Node.js syllabus and module examples with utility functions

---
+++ Modules/index.js
@@ -0,0 +1,9 @@
+// console.log("Lets Learn about modules in NodeJS")
+// const {add,subtract,multiply,divide}=require('./utils')
+
+import {add,subtract,multiply,divide} from './utils.js'
+
+console.log(add(10,20));
+console.log(subtract(10,20));
+console.log(multiply(10,20));
+console.log(divide(10,20));
