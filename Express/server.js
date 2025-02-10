19beaa8a111088b667d681edc2e619b811e6a260
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-02-10 13:23:56 +0530

    Done with the express and routing

--- Express/server.js
+++ Express/server.js
@@ -2,5 +2,21 @@
 import express from 'express';
 
 const app=express();
+const PORT=3000;
 
+//Routing 
+app.get("/",(req,res)=>{
+    res.send("Welcome to Home Page")
+})
 
+app.get("/about",(req,res)=>{
+    res.send("Welcome to about page")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to contact page")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+})
